d298b5320a51101d4e5b4c6ffc177e61a9f5885e
Gokhan <genel@gokhan.biz>
2021-05-30 12:33:44 +0300

    commets

--- FireImpact/Program.cs
+++ FireImpact/Program.cs
@@ -7,6 +7,29 @@ namespace FireImpact
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            //Create a weapon
+            Weapon weapon = new Weapon(muzzleCaliber: 40, muzzleDir: new Vector3D(1, 0, 0), muzzlePos: new Vector3D(0, 0, 2));
+
+            //Create Effect Areas as shown at the documentation image
+            EffectArea eaFlash = new EffectArea(ref weapon, rotation: 0, lengthFactor: 50, impactFactorAtConeTip: 1, impactFactorAtConeBase: 0.8);
+            EffectArea eaHigh = new EffectArea(ref weapon, rotation: 0, lengthFactor: 250, impactFactorAtConeTip: 1, impactFactorAtConeBase: 0.4);
+            EffectArea eaLow_1 = new EffectArea(ref weapon, rotation: 90, lengthFactor: 150, impactFactorAtConeTip: 0.7, impactFactorAtConeBase: 0.2);
+            EffectArea eaLow_2 = new EffectArea(ref weapon, rotation: -90, lengthFactor: 150, impactFactorAtConeTip: 0.7, impactFactorAtConeBase: 0.2);
+
+            //Attach Effect Areas to weapon
+            List<EffectArea> effectAreas = new List<EffectArea>() { eaFlash, eaHigh, eaLow_1, eaLow_2 };
+            weapon.effectAreas = effectAreas;
+
+            //Create some randomly placed soldiers - X and Y values will be in range of -12 and 12, z = 1.8 
+            GameUtils.CreateTestUnits(count: 30, maxDistance: 12);
+
+            //TODO: weapon fire
+            ApplyBlindnessAndDeafness(GameUtils.allUnits, weapon);
+
+
+        }
 
         /// <summary>
         /// This method iterates <paramref name="allUnits"/> and applies deafness and blindness if necessary.
@@ -58,40 +81,28 @@ namespace FireImpact
 
         }
 
-        private static double GetDuration(Soldier soldier, EffectArea zone, double maxDuration)
-        {
-            var distance = Math.Abs((soldier.Positon - zone.Weapon.muzzlePos).Length);
-            var distanceRate = distance / zone.GetLength();
-            var impactFactor = (zone.ImpactFactorAtConeTip - zone.ImpactFactorAtConeBase) * distanceRate + zone.ImpactFactorAtConeBase;
 
-            return impactFactor * maxDuration; ;
+        /// <summary>
+        /// Calculates durations for blindness/deafness.
+        /// 
+        /// </summary>
+        /// <param name="soldier">Soldier who will be blinded/deafened</param>
+        /// <param name="effecArea">Effect Area that soldier is in.</param>
+        /// <param name="maxDuration">Maximum Duration for blindness or deafness effect.</param>
+        /// <returns></returns>
+        private static double GetDuration(Soldier soldier, EffectArea effecArea, double maxDuration)
+        {
+            var distance = Math.Abs((soldier.Positon - effecArea.Weapon.muzzlePos).Length);
+            var distanceRate = distance / effecArea.GetLength();
+            var impactFactor = (effecArea.ImpactFactorAtConeTip - effecArea.ImpactFactorAtConeBase) * distanceRate + effecArea.ImpactFactorAtConeBase;
+
+            //If somehow an improper impactFactor is entered for the zone, that will prevent exceeding maximum duration allowed.
+            impactFactor = impactFactor > 1 ? 1 : impactFactor; 
+
+            return impactFactor * maxDuration;
         }
 
-        static void Main(string[] args)
-        {
-            //Create a weapon
-            Weapon weapon = new Weapon(muzzleCaliber: 40, muzzleDir: new Vector3D(1, 0, 0), muzzlePos: new Vector3D(0, 0, 2));
-
-
-            //Create Effect Areas as shown at the documentation image
-            EffectArea eaFlash = new EffectArea(ref weapon, rotation: 0, lengthFactor: 50, impactFactorAtConeTip: 1, impactFactorAtConeBase: 0.8);
-            EffectArea eaHigh = new EffectArea(ref weapon, rotation: 0, lengthFactor: 250, impactFactorAtConeTip: 1, impactFactorAtConeBase: 0.4);
-            EffectArea eaLow_1 = new EffectArea(ref weapon, rotation: 90, lengthFactor: 150, impactFactorAtConeTip: 0.7, impactFactorAtConeBase: 0.2);
-            EffectArea eaLow_2 = new EffectArea(ref weapon, rotation: -90, lengthFactor: 150, impactFactorAtConeTip: 0.7, impactFactorAtConeBase: 0.2);
-
-            //Attach Effect Areas to weapon
-            List<EffectArea> effectAreas = new List<EffectArea>() { eaFlash, eaHigh, eaLow_1, eaLow_2 };
-            weapon.effectAreas = effectAreas;
-
-            //Create some soldiers
-            GameUtils.CreateTestUnits(30, 12);
-
-
-            //TODO: weapon fire
-            ApplyBlindnessAndDeafness(GameUtils.allUnits, weapon);
-
-
-        }
+      
     }
 
 
@@ -219,39 +230,55 @@ namespace FireImpact
     }
 
 
-
+    /// <summary>
+    /// This is a helper class to simulate data will be fetched from Ganme Environment.
+    /// </summary>
     static class GameUtils
     {
-
+        /// <summary>
+        /// SetBlindness funciton as stated in the Co
+        /// </summary>
+        /// <param name="soldier">Soldier who will be blinded.</param>
+        /// <param name="duration">Duration of Blindness</param>
+        /// <remarks>Writes info about soldier and duration to console</remarks>
         public static void SetBlindness(Soldier soldier, double duration)
         {
+            if(duration > 0)
             Console.WriteLine("Soldier_{0} will be BLIND for next {1} seconds", soldier.Id, duration);
         }
 
+        /// <summary>
+        /// SetBlindness funciton as stated in the Co
+        /// </summary>
+        /// <param name="soldier">Soldier who will be blinded.</param>
+        /// <param name="duration">Duration of Deafness</param>
+        /// <remarks>Writes info about soldier and duration to console</remarks>
         public static void SetDeafness(Soldier soldier, double duration)
         {
             Console.WriteLine("Soldier_{0} will be DEAF for next {1} seconds", soldier.Id, duration);
 
         }
 
+
         public const double MaxBlindnessDuration = 2.0d;
         public const double MaxDeafnessDuration = 60.0d;
 
+        //Array of all soldiers in Game Environmetn.. 
         public static Soldier[] allUnits;
 
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="count"></param>
-        /// <param name="maxDistance"></param>
+        /// <param name="count">Number of soldiers that will be initialized.</param>
+        /// <param name="maxDistance"> X and Y values of soldier positon will </param>
         public static void CreateTestUnits(int count, int maxDistance)
         {
 
             allUnits = new Soldier[count];
             Random r = new Random();
 
-            var z = 1.8; //all soldiers considered standing.
+            var z = 1.8; //all soldiers are considered as standing, and they are 1.8 meters.
 
             for (int i = 0; i < count; i++)
             {
@@ -266,7 +293,9 @@ namespace FireImpact
     }
 
 
-
+    /// <summary>
+    /// <c>Soldier</c> class   . Soldiers are represented as point.
+    /// </summary>
     public class Soldier
     {
         public int Id { get; set; }
@@ -277,7 +306,6 @@ namespace FireImpact
             Id = id;
             Positon = positon;
         }
-
 
         // standing ducked crawling
 
